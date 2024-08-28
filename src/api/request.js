d7b7a1a06a010c71fd79d4a14de4bb084ec5b733
buxinran <141600249+buxinran@users.noreply.github.com>
2024-08-28 22:01:50 +0800

    q

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create(
   {
-    baseURL: "http://localhost:9092/api", //这里配置的是后端服务提供的接口
+    baseURL: "http://localhost:30005/api", //这里配置的是后端服务提供的接口
   }
 );
 export default api;
